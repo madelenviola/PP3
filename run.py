e9645c8de180e81d661ca58590d7f44693adb0fd
madelenviola <>
2023-05-22 18:41:13 +0000

    fix date intervals

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-date_input = input ("Please enter month and date of birth (MM/DD):")
+date_input = input ("Please enter month and date of birth (MM/DD): ")
 
 month, day = date_input.split('/')
 
-if (month == "03" and 21 <= int(day) <=31) or (month == "04" and 1 <= int(day) <=19):
+if (month == '03' and 21 <= int(day)) or (month == '04' and int(day) <=19):
     print("You are a Aries")
+elif (month == '04' and 20 <= int(day)) or (month == '05' and int(day) <=20):
+    print("You are a Taurus")
+elif (month == '05' and 21 <= int(day)) or (month == '06' and int(day) <=21):
+    print("You are a Gemini")
