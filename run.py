260a81bc483117294ae4ea88e074d2475221b8c5
madelenviola <>
2023-05-22 16:22:35 +0000

    Add first if statement to check date interval and  print message

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-date_input = input ("Please enter month and day of birth (MM/DD):")
+date_input = input ("Please enter month and date of birth (MM/DD):")
+
+month, day = date_input.split('/')
+
+if (month == "03" and 21 <= int(day) <=31) or (month == "04" and 1 <= int(day) <=19):
+    print("You are a Aries")
