a73895fe36e89ffb6c4965adf5fe5393e934d15a
madelenviola <>
2023-05-22 20:15:59 +0000

    boiler plate for key options and interval messages added per interval

--- run.py
+++ run.py
@@ -7,43 +7,66 @@ key_pressed1 = ""
 key_pressed2 = ""
 
 if (month == '03' and 21 <= int(day)) or (month == '04' and int(day) <=19):
-    interval_message "You're a Aries"
+    interval_message = "You're a Aries"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '04' and 20 <= int(day)) or (month == '05' and int(day) <=20):
-    interval_message "You're a Taurus"
+    interval_message = "You're a Taurus"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '05' and 21 <= int(day)) or (month == '06' and int(day) <=21):
-    interval_message "You're a Gemini"
+    interval_message = "You're a Gemini"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '06' and 22 <= int(day)) or (month == '07' and int(day) <=22):
-    interval_message "You're a Cancer"
+    interval_message = "You're a Cancer"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '07' and 23 <= int(day)) or (month == '08' and int(day) <=22):
-    interval_message "You're a Leo"
+    interval_message = "You're a Leo"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '08' and 23 <= int(day)) or (month == '09' and int(day) <=22):
-    interval_message "You're a Virgo"
+    interval_message = "You're a Virgo"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '09' and 23 <= int(day)) or (month == '10' and int(day) <=23):
-    print("You are a Leo")
-    interval_message "You're a Leo"
+    interval_message = "You're a Leo"
     key_pressed1 = "This is option 1"
     key_pressed2 = "This is option 2"
 elif (month == '10' and 24 <= int(day)) or (month == '11' and int(day) <=21):
-    print("You are a Scorpio")
+    interval_message = "You're a Scorpio"
+    key_pressed1 = "This is option 1"
+    key_pressed2 = "This is option 2"
 elif (month == '11' and 22 <= int(day)) or (month == '12' and int(day) <=21):
-    print("You are a Sagittarius")
+    interval_message = "You're a Sagittarius"
+    key_pressed1 = "This is option 1"
+    key_pressed2 = "This is option 2"
 elif (month == '12' and 22 <= int(day)) or (month == '01' and int(day) <=19):
-    print("You are a Capricorn")
+    interval_message = "You're a Capricorn"
+    key_pressed1 = "This is option 1"
+    key_pressed2 = "This is option 2"
 elif (month == '01' and 20 <= int(day)) or (month == '02' and int(day) <=18):
-    print("You are a Aquarius")
+    interval_message = "You're a Aquarius"
+    key_pressed1 = "This is option 1"
+    key_pressed2 = "This is option 2"
 elif (month == '02' and 19 <= int(day)) or (month == '03' and int(day) <=20):
-    print("You are a Pisces")
+    interval_message = "You're a Pisces"
+    key_pressed1 = "This is option 1"
+    key_pressed2 = "This is option 2"
+
+if interval_message:
+    print(interval_message)
+    print(key_pressed1)
+    print(key_pressed2)
+
+    key = input ("Press '1' for option 1, or '2' for option 2: ")
+
+    if key == '1':
+        print(key_pressed1)
+    elif key == '2':
+        print(key_pressed2)
+    else:
+        print("Please press '1' or '2'. ")
 else: 
     print("Invalid format please add birth month and date MM/DD")
