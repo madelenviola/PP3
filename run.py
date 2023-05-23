9f7df15ff4a553a136a0b543371e1da567732a34
madelenviola <>
2023-05-23 18:34:48 +0000

    Bug fix print invalid input message when using letters

--- run.py
+++ run.py
@@ -89,75 +89,53 @@ StarSign.PISCES: {
     },       
 }
 
-date_input = input ("Please enter month and date of birth (MM/DD): ")
+def _validate_birth_date(date_input):
+    date_parts = date_input.split("/")
+    if len(date_parts) == 2:
+        month, day = date_parts
+
+        if month.isdigit() and day.isdigit():
+            month = int(month)
+            day = int(day)
+
+            if 1 <= month <= 12 and 1 <= day <= 31:
+                return True
+
+    return False
+
+date_input = input("Please enter month and date of birth (MM/DD): ")
+
+while not _validate_birth_date(date_input):
+    if not date_input.replace('/', '').isdigit():
+        print("Input not valid. Please enter a valid birth date in the format MM/DD")
+        date_input = input("Enter month and date of birth in format (MM/DD): ")
 
 month, day = date_input.split('/')
 
-interval_message = ""
-key_pressed1 = ""
-key_pressed2 = ""
-key_pressed3 = ""
-
 if (month == '03' and 21 <= int(day)) or (month == '04' and int(day) <=19):
-    interval_message = "You're a Aries"
-    key_pressed1 = "Best career match for Aries is:"
-    key_pressed2 = "How to succeed as a Aries:"
-    key_pressed3 = "Aries Celbrities:"
+    star_sign = StarSign.ARIES
 elif (month == '04' and 20 <= int(day)) or (month == '05' and int(day) <=20):
     star_sign = StarSign.TAURUS
-    key_pressed1 = "Best career match for Taurus is: Chef, Artist, Interior Designer, Architect, Construction, Politician, Fashion Designer, Botanist, Biologist. "
-    key_pressed2 = "How to succeed as a Taurus: You are someone people can count on, and that is super important. At times when people don't follow through on things, you know and understand how crucial it is to keep your word. Your weaknesses of being very obstinate and overprotective can fall heavy on you. You can also be stubborn to the point of not compromising which is a major rule-breaker for success."
-    key_pressed3 = "Taurus Celbrities: Elizabeth II, Adele, Robert Pattinson, The Rock, George Clooney, Tina Fey, Channing Tatum, Jessica Alba, Dev Patel to name a few"
 elif (month == '05' and 21 <= int(day)) or (month == '06' and int(day) <=21):
-    interval_message = "You're a Gemini"
-    key_pressed1 = "Best career match for Gemini is:"
-    key_pressed2 = "How to succeed as a Gemini:"
-    key_pressed3 = "Gemini Celbrities:"
+    star_sign = StarSign.GEMINI
 elif (month == '06' and 22 <= int(day)) or (month == '07' and int(day) <=22):
-    interval_message = "You're a Cancer"
-    key_pressed1 = "Best career match for Cancer is:"
-    key_pressed2 = "How to succeed as a Cancer:"
-    key_pressed3 = "Cancer Celbrities:"
+    star_sign = StarSign.CANCER
 elif (month == '07' and 23 <= int(day)) or (month == '08' and int(day) <=22):
-    interval_message = "You're a Leo"
-    key_pressed1 = "Best career match for Leo is:"
-    key_pressed2 = "How to succeed as a Leo:"
-    key_pressed3 = "Leo Celbrities:"
+    star_sign = StarSign.LEO
 elif (month == '08' and 23 <= int(day)) or (month == '09' and int(day) <=22):
-    interval_message = "You're a Virgo"
-    key_pressed1 = "Best career match for Virgo is:"
-    key_pressed2 = "How to succeed as a Virgo:"
-    key_pressed3 = "Virgo Celbrities:"
+    star_sign = StarSign.VIRGO
 elif (month == '09' and 23 <= int(day)) or (month == '10' and int(day) <=23):
-    interval_message = "You're a Leo"
-    key_pressed1 = "Best career match for leo is:"
-    key_pressed2 = "How to succeed as a Leo:"
-    key_pressed3 = "Leo Celbrities:"
+    star_sign = StarSign.LIBRA
 elif (month == '10' and 24 <= int(day)) or (month == '11' and int(day) <=21):
-    interval_message = "You're a Scorpio"
-    key_pressed1 = "Best career match for Scorpio is:"
-    key_pressed2 = "How to succeed as a Scporpio:"
-    key_pressed3 = "Scorpio Celbrities:"
+    star_sign = StarSign.SCORPIO
 elif (month == '11' and 22 <= int(day)) or (month == '12' and int(day) <=21):
-    interval_message = "You're a Sagittarius"
-    key_pressed1 = "Best career match for Sagittarius is:"
-    key_pressed2 = "How to succeed as a Sagittarius:"
-    key_pressed3 = "Sagittarius Celbrities:"
+    star_sign = StarSign.SAGITTARIUS
 elif (month == '12' and 22 <= int(day)) or (month == '01' and int(day) <=19):
-    interval_message = "You're a Capricorn"
-    key_pressed1 = "Best career match for Capricorn is:"
-    key_pressed2 = "How to succeed as a Capricorn:"
-    key_pressed3 = "Capricorn Celbrities:"
+    star_sign = StarSign.CAPRICORN
 elif (month == '01' and 20 <= int(day)) or (month == '02' and int(day) <=18):
-    interval_message = "You're a Aquarius"
-    key_pressed1 = "Best career match for Aquarius is:"
-    key_pressed2 = "How to succeed as a Aquarius:"
-    key_pressed3 = "Aquarius Celbrities:"
+    star_sign = StarSign.AQUARIUS
 elif (month == '02' and 19 <= int(day)) or (month == '03' and int(day) <=20):
-    interval_message = "You're a Pisces"
-    key_pressed1 = "Best career match for Pisces is:"
-    key_pressed2 = "How to succeed as a Pisces:"
-    key_pressed3 = "Pisces Celbrities:"
+    star_sign = StarSign.PISCES
     print("Star Sign: ", star_sign.value)
 
 
